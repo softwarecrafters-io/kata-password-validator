79c7d3319ea244e3f5a42d11e98bb8005fcf5a30
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-25 11:54:23 +0000

    red: 'fails when the password is too short'

--- src/tests/passwordValidator.test.ts
+++ src/tests/passwordValidator.test.ts
@@ -12,4 +12,7 @@ describe('The password strength validator', () => {
 	it('considers a password to be strong when all requirements are met', () => {
 		expect(isStrongPassword('1234abcdABCD_')).toBe(true);
 	});
+	it('fails when the password is too short', () => {
+		expect(isStrongPassword('1aA_')).toBe(false);
+	});
 });
