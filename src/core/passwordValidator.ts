df5a4e92260d620572484e37a5e1393e42bf4c5a
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-25 13:10:34 +0000

    refactor: extrad methods

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,3 +1,11 @@
 export function isStrongPassword(password: string) {
-	return password.length >= 6 && /.*\d.*/.test(password);
+	return hasSixCharactersOrMore(password) && containsNumber(password);
+}
+
+function hasSixCharactersOrMore(password: string) {
+	return password.length >= 6;
+}
+
+function containsNumber(password: string) {
+	return /.*\d.*/.test(password);
 }
