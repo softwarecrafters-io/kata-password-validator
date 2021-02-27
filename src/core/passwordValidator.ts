770e00953009bf10aa106ffad811d1d80907b2cb
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-02-27 11:59:53 +0000

    refactor: extract function, rename

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -2,9 +2,9 @@ export function isStrongPassword(password: string) {
 	return (
 		hasSixCharactersOrMore(password) &&
 		containsNumber(password) &&
-		constainsLowerCase(password) &&
-		constainsUpperCase(password) &&
-		password.includes('_')
+		containsLowerCase(password) &&
+		containsUpperCase(password) &&
+		containsUnderscore(password)
 	);
 }
 
@@ -16,10 +16,14 @@ function containsNumber(password: string) {
 	return /.*\d.*/.test(password);
 }
 
-function constainsLowerCase(password: string) {
+function containsLowerCase(password: string) {
 	return /.*[a-z].*/.test(password);
 }
 
-function constainsUpperCase(password: string) {
+function containsUpperCase(password: string) {
 	return /.*[A-Z].*/.test(password);
+}
+
+function containsUnderscore(password: string) {
+	return password.includes('_');
 }
